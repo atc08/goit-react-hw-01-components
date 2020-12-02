27a92131a461d691917c6b291bab4c2f5962f947
Yevhenii Korkots <atc0884@gmail.com>
2020-12-02 19:48:43 +0200

    folders added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Profile from './components/Profile';
-import Statistics from './components/Statistics';
-import FriendList from './components/FriendList';
-import TransactionHistory from './components/TransactionHistory';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import Section from './components/Section';
 import user from './incomingData/user.json';
 import statisticalData from './incomingData/statistical-data.json';
-import friends from './incomingData/friends.json'
-import transactions from './incomingData/transactions.json'
+import friends from './incomingData/friends.json';
+import transactions from './incomingData/transactions.json';
 
 export default function App() {
   return (
@@ -20,25 +20,18 @@ export default function App() {
           stats={user.stats}
         />
       </Section>
-      
+
       <Section title="Задание 2 - Секция статистики">
-        <Statistics
-          title="Upload stats"
-          stats={statisticalData}
-        />
+        <Statistics title="Upload stats" stats={statisticalData} />
       </Section>
-      
+
       <Section title="Задание 3 - Список друзей">
-        <FriendList
-          friends={friends}
-        />
+        <FriendList friends={friends} />
       </Section>
-      
+
       <Section title="Задание 4 - История транзакций">
-        <TransactionHistory
-          items={transactions}
-        />
-      </Section>  
+        <TransactionHistory items={transactions} />
+      </Section>
     </div>
   );
 }
